bd459ff529b7f8531b8ec28a1bd40dd048c1b349
Akshay Srivastava <akshay.srivastava.95@gmail.com>
2017-01-22 17:03:17 +0530

    Created Schema

--- src/main/java/com/ait/apex/row/Schema.java
+++ src/main/java/com/ait/apex/row/Schema.java
@@ -1,7 +1,19 @@
 package com.ait.apex.row;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Schema
 {
+	List<RowMeta> rowMetaList = new ArrayList<>();
 	
+	void addField(String name, DataType dataType)
+	{
+		RowMeta rowMeta = new RowMeta();
+		rowMeta.setName(name);
+		rowMeta.setDataType(dataType);
+		
+		rowMetaList.add(rowMeta);
+	}
 }
