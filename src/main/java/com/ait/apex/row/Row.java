8a98e4beca8478761e75750357b90c42ba107c15
Akshay Srivastava <akshay.srivastava.95@gmail.com>
2017-01-26 19:12:58 +0530

    Fixed Test

--- src/main/java/com/ait/apex/row/Row.java
+++ src/main/java/com/ait/apex/row/Row.java
@@ -3,6 +3,7 @@ package com.ait.apex.row;
 public class Row
 {
 	private int offset;
+	private int basicByte = 8;
 	private byte[] keyBytes[];
 	private byte[] valueBytes[];
 	
--- src/test/java/com/ait/apex/PlatformTest.java
+++ src/test/java/com/ait/apex/PlatformTest.java
@@ -1,6 +1,7 @@
 package com.ait.apex;
 
 import com.ait.apex.platform.Platform;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -12,15 +13,17 @@ public class PlatformTest
 	{
 		byte[] data = new byte[25];
 		System.out.println(Arrays.toString(data));
-		Platform.putLong(data,Platform.LONG_ARRAY_OFFSET, 123124);
+		Platform.putLong(data,Platform.LONG_ARRAY_OFFSET, Long.MAX_VALUE);
 		System.out.println(Arrays.toString(data));
-		Platform.putDouble(data, Platform.DOUBLE_ARRAY_OFFSET+1, 347.1);
+		Platform.putDouble(data, Platform.DOUBLE_ARRAY_OFFSET + 8, Double.MAX_VALUE);
 		System.out.println(Arrays.toString(data));
-		Platform.putInt(data, Platform.INT_ARRAY_OFFSET+2, 1234);
+		Platform.putInt(data, Platform.INT_ARRAY_OFFSET + 16, 1234);
 		System.out.println(Arrays.toString(data));
 		System.out.println(Platform.getLong(data, Platform.LONG_ARRAY_OFFSET));
-		System.out.println(Platform.getDouble(data, Platform.DOUBLE_ARRAY_OFFSET+1));
-		System.out.println(Platform.getInt(data, Platform.INT_ARRAY_OFFSET+2));
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Platform.getDouble(data, Platform.DOUBLE_ARRAY_OFFSET + 8));
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Platform.getInt(data, Platform.INT_ARRAY_OFFSET + 16));
 	}
 	
 	/*
