36bbdc16a2134f8672c86ef3d729a2434a1fcdd4
Vishal Vir Singh <jaswal2868@gmail.com>
2017-03-01 10:54:49 +0530

    created BytePlatform

---
+++ src/main/java/com/ait/apex/platform/BytePlatform.java
@@ -0,0 +1,87 @@
+package com.ait.apex.platform;
+
+import java.nio.ByteBuffer;
+
+public class BytePlatform {
+
+    public static int getInt(Object object, long offset)
+    {
+     ByteBuffer bb = (ByteBuffer)object;
+     return bb.getInt((int) offset);
+    }
+
+    public static void putInt(Object object, long offset, int value)
+    {
+        ByteBuffer bb = (ByteBuffer)object;
+        bb.putInt(value);
+    }
+
+   /* public static boolean getBoolean(Object object, long offset)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        return bb.getBoolean(object, offset);
+    }
+
+    public static void putBoolean(Object object, long offset, boolean value)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        bb.putBoolean(object, offset, value);
+    }
+
+    public static byte getByte(Object object, long offset)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        return bb.getByte(object, offset);
+    }
+
+    public static void putByte(Object object, long offset, byte value)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        bb.putByte(object, offset, value);
+    }
+    */
+
+    public static short getShort(Object object, long offset)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        return bb.getShort((int)offset);
+    }
+
+    public static void putShort(Object object, long offset, short value) {
+        ByteBuffer bb = (ByteBuffer) object;
+        bb.putShort((int) offset, value);
+
+    }
+
+    public static long getLong(Object object, long offset)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        return bb.getLong((int)offset);
+    }
+
+    public static void putLong(Object object, long offset, long value)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        bb.putLong((int)offset,value);
+    }
+
+    public static float getFloat(Object object, long offset)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        return bb.getFloat((int)offset);
+    }
+
+    public static void putFloat(Object object, long offset, float value)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        bb.putFloat((int)offset,value);
+    }
+
+    public static double getDouble(Object object, long offset)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        return bb.getDouble((int)offset);
+    }
+
+    public static void putDouble(Object object, long offset, double value)
+    {   ByteBuffer bb = (ByteBuffer)object;
+        bb.putDouble((int)offset,value);
+    }
+
+
+    public static ByteBuffer memoryAllocate(int pagesize)
+    {
+
+      return ByteBuffer.allocateDirect(pagesize);
+    }
+}
