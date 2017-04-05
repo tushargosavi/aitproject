248b3ac67de6bb5fa32f960d18f27e44fe91965c
Akshay Srivastava <akshay.srivastava.95@gmail.com>
2017-04-05 21:18:11 +0530

    Updated Coder and ByteLength

--- src/main/java/com/ait/apex/row/PojoBasedCoder.java
+++ src/main/java/com/ait/apex/row/PojoBasedCoder.java
@@ -1,62 +1,121 @@
 package com.ait.apex.row;
 
 
+import com.ait.apex.aggregator.meta.AggregationSchema;
 import com.ait.apex.platform.Platform;
 
-public class PojoBasedCoder implements Coder{
+public class PojoBasedCoder{
 	
 	VariableFunctions variableFunctions = new VariableFunctions();
-	ByteLength length = new ByteLength();
-
-	@Override
-	public Row encoder(RowMeta rowMeta, Object o) throws NoSuchFieldException, IllegalAccessException {
-		Row row = new Row();
-		long offset = 0;
-		int varoffset = length.getVarOffset(rowMeta);
-		int size = length.getByteLength(rowMeta, o);
-		row.dataBytes = new byte[size];
+	ByteLength byteLength = new ByteLength();
+	
+	public Row encoder(AggregationSchema schema, Object object) throws NoSuchFieldException, IllegalAccessException
+	{
 		
-		for (FieldInfo fieldInfo : rowMeta.getFieldInfoList()) {
-			switch (fieldInfo.getDataType()) {
+		Row row = new Row();
+		row.dataBytes = new byte[byteLength.getRowLength(schema, object)];
+		int offset = 0;
+		
+		//Total length
+		Platform.putInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset, byteLength.getRowLength(schema, object));
+		offset += 4;
+		
+		//Key length
+		Platform.putInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset, byteLength.getKeyLength(schema.keySchema, object));
+		offset += 4;
+		
+		//Value length
+		Platform.putInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset, byteLength.getValLength(schema.valueSchema, object));
+		offset += 4;
+		
+		int keyVarOffset = byteLength.getKeyVarOffset(schema.keySchema);
+		
+		for (FieldInfo fieldInfo : schema.keySchema.getFieldInfoList())
+		{
+			switch (fieldInfo.getDataType())
+			{
 				case STRING:
-					String str = (String) o.getClass().getField(fieldInfo.getName()).get(o);
- 					variableFunctions.putString(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset, varoffset, str);
+					String str = (String) object.getClass().getField(fieldInfo.getName()).get(object);
+					variableFunctions.putString(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset, keyVarOffset, str);
 					offset += 8;
-					varoffset +=  str.getBytes().length;
+					offset += str.getBytes().length;
+					keyVarOffset += str.getBytes().length;
 					break;
 				
 				case INTEGER:
-					int intVal = (int) o.getClass().getField(fieldInfo.getName()).get(o);
+					int intVal = (int) object.getClass().getField(fieldInfo.getName()).get(object);
 					Platform.putInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset, intVal);
 					offset += 4;
 					break;
 				
 				case LONG:
-					long longVal = (long) o.getClass().getField(fieldInfo.getName()).get(o);
+					long longVal = (long) object.getClass().getField(fieldInfo.getName()).get(object);
 					Platform.putLong(row.dataBytes, Platform.LONG_ARRAY_OFFSET + offset, longVal);
 					offset += 8;
 					break;
 				
 				case CHARACTER:
-					char charVal = (char) o.getClass().getField(fieldInfo.getName()).get(o);
+					char charVal = (char) object.getClass().getField(fieldInfo.getName()).get(object);
 					variableFunctions.putChar(row.dataBytes, Platform.BYTE_ARRAY_OFFSET + offset, charVal);
 					offset += 2;
 					break;
 				
 				case DOUBLE:
-					double doubleVal = (double) o.getClass().getField(fieldInfo.getName()).get(o);
+					double doubleVal = (double) object.getClass().getField(fieldInfo.getName()).get(object);
 					Platform.putDouble(row.dataBytes, Platform.DOUBLE_ARRAY_OFFSET + offset, doubleVal);
 					offset += 8;
 					break;
 				
 				case BOOLEAN:
-					boolean boolval = (boolean) o.getClass().getField(fieldInfo.getName()).get(o);
+					boolean boolval = (boolean) object.getClass().getField(fieldInfo.getName()).get(object);
 					Platform.putBoolean(row.dataBytes, Platform.BOOLEAN_ARRAY_OFFSET + offset, boolval);
 					offset += 1;
 					break;
 				
 				case FLOAT:
-					float floatVal = (float) o.getClass().getField(fieldInfo.getName()).get(o);
+					float floatVal = (float) object.getClass().getField(fieldInfo.getName()).get(object);
+					Platform.putFloat(row.dataBytes, Platform.FLOAT_ARRAY_OFFSET + offset, floatVal);
+					offset += 4;
+					break;
+			}
+		}
+		
+		
+		for (FieldInfo fieldInfo : schema.valueSchema.getFieldInfoList())
+		{
+			switch (fieldInfo.getDataType()) {
+				case INTEGER:
+					int intVal = (int) object.getClass().getField(fieldInfo.getName()).get(object);
+					Platform.putInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset, intVal);
+					offset += 4;
+					break;
+				
+				case LONG:
+					long longVal = (long) object.getClass().getField(fieldInfo.getName()).get(object);
+					Platform.putLong(row.dataBytes, Platform.LONG_ARRAY_OFFSET + offset, longVal);
+					offset += 8;
+					break;
+				
+				case CHARACTER:
+					char charVal = (char) object.getClass().getField(fieldInfo.getName()).get(object);
+					variableFunctions.putChar(row.dataBytes, Platform.BYTE_ARRAY_OFFSET + offset, charVal);
+					offset += 2;
+					break;
+				
+				case DOUBLE:
+					double doubleVal = (double) object.getClass().getField(fieldInfo.getName()).get(object);
+					Platform.putDouble(row.dataBytes, Platform.DOUBLE_ARRAY_OFFSET + offset, doubleVal);
+					offset += 8;
+					break;
+				
+				case BOOLEAN:
+					boolean boolval = (boolean) object.getClass().getField(fieldInfo.getName()).get(object);
+					Platform.putBoolean(row.dataBytes, Platform.BOOLEAN_ARRAY_OFFSET + offset, boolval);
+					offset += 1;
+					break;
+				
+				case FLOAT:
+					float floatVal = (float) object.getClass().getField(fieldInfo.getName()).get(object);
 					Platform.putFloat(row.dataBytes, Platform.FLOAT_ARRAY_OFFSET + offset, floatVal);
 					offset += 4;
 					break;
@@ -65,58 +124,38 @@ public class PojoBasedCoder implements Coder{
 		return row;
 	}
 	
-	
-	@Override
-	public Object decoder(RowMeta rowMeta, Row row, Object object) throws NoSuchFieldException, IllegalAccessException
+	public Row extractKeyRow(Row row)
 	{
-		int offset = 0;
-		for(FieldInfo fieldInfo : rowMeta.getFieldInfoList())
+		
+		int length = Platform.getInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + 4);
+		Row resultRow = new Row();
+		resultRow.dataBytes = new byte[length];
+		for(int i = 0; i < length; i++)
 		{
-			switch (fieldInfo.getDataType())
-			{
-				case STRING:
-					String strVal = variableFunctions.getString(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset);
-					object.getClass().getField(fieldInfo.getName()).set(object, strVal);
-					offset += 8;
-					break;
-
-				case INTEGER:
-					int intVal = Platform.getInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + offset);
-					object.getClass().getField(fieldInfo.getName()).set(object, intVal);
-					offset += 4;
-					break;
-
-				case LONG:
-					long longVal = Platform.getLong(row.dataBytes, Platform.LONG_ARRAY_OFFSET + offset);
-					object.getClass().getField(fieldInfo.getName()).set(object, longVal);
-					offset += 8;
-					break;
-
-				case CHARACTER:
-					char charVal = variableFunctions.getChar(row.dataBytes, Platform.BYTE_ARRAY_OFFSET + offset);
-					object.getClass().getField(fieldInfo.getName()).set(object, charVal);
-					offset += 2;
-					break;
-				
-				case DOUBLE:
-					double doubleVal = Platform.getDouble(row.dataBytes, Platform.DOUBLE_ARRAY_OFFSET + offset);
-					object.getClass().getField(fieldInfo.getName()).set(object, doubleVal);
-					offset += 8;
-					break;
-				
-				case BOOLEAN:
-					boolean boolVal = Platform.getBoolean(row.dataBytes, Platform.BOOLEAN_ARRAY_OFFSET + offset);
-					object.getClass().getField(fieldInfo.getName()).set(object, boolVal);
-					offset += 8;
-					break;
-				
-				case FLOAT:
-					float floatVal = Platform.getFloat(row.dataBytes, Platform.FLOAT_ARRAY_OFFSET + offset);
-					object.getClass().getField(fieldInfo.getName()).set(object, floatVal);
-					break;
-			}
+			resultRow.dataBytes[i] = Platform.getByte(row.dataBytes, Platform.BYTE_ARRAY_OFFSET + 12 + i);
 		}
-
-		return object;
+		return resultRow;
+	}
+	
+	public Row extractValRow(Row row)
+	{
+		int length = Platform.getInt(row.dataBytes, Platform.INT_ARRAY_OFFSET + 8);
+		int startIndex = Platform.getInt(row.dataBytes, Platform.INT_ARRAY_OFFSET) - length;
+		Row resultRow = new Row();
+		resultRow.dataBytes = new byte[length];
+		for(int i = 0; i < length; i++)
+		{
+			resultRow.dataBytes[i] = Platform.getByte(row.dataBytes, Platform.BYTE_ARRAY_OFFSET + startIndex + i);
+		}
+		return resultRow;
+	}
+	
+	public void updateRow(Row oldRow, byte[] updatedData, int length)
+	{
+		int startIndex = Platform.getInt(oldRow.dataBytes, Platform.INT_ARRAY_OFFSET) - length;
+		for(int i = 0; i < length ;i++)
+		{
+			Platform.putInt(oldRow.dataBytes, Platform.BYTE_ARRAY_OFFSET + startIndex + i, updatedData[i]);
+		}
 	}
 }
